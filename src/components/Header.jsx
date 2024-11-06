590cfc62b73907fd498b051d0ba466b85fd295c8
Khayam Ijaz <ijazkhayam@gmail.com>
2024-11-06 10:26:26 +0500

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ import { FaGithub } from "react-icons/fa6";
 const Header = ({isClick}) => {
   return (
     <>
-       <div onClick={isClick} className="flex rightHead items-center justify-center">
-        <RiMenu2Line className='menuIcon hidden' size={25} cursor="pointer" /> 
+       <div className="flex rightHead items-center justify-center">
+        <RiMenu2Line onClick={isClick} className='menuIcon hidden' size={25} cursor="pointer" /> 
       <h1 className='text-center md:text-[4vw] text-[5vw] font-semibold font-["Poppins", san-serif]'>React Buttons Library</h1>
         <FaGithub className='gitIcon hidden' size={30} cursor="pointer" />
       </div>
